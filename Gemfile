2fd4f19381283ab1b1d3b147eb05fc739b5d76e4
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-08-26 23:42:50 +0000

    hash is working

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ ruby "3.2.1"
 
 gem "sinatra"
 gem "sinatra-contrib"
+gem "openai"
+gem "ruby-openai"
+gem "json"
+gem "http"
 
 # Use Puma as the app server
 gem "puma", "~> 5.0"
--- app.rb
+++ app.rb
@@ -1,9 +1,89 @@
 require "sinatra"
 require "sinatra/reloader"
+require "openai"
+require "http"
+require "json"
+
+
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+ erb(:chat, {:layout => :layout})
+end
+
+get("/list") do
+  
+
+ 
+
+  erb(:events_chat, {:layout => :layout})
+end
+
+post("/list")do
+  # Call the API to get the next message from GPT
+  # client = OpenAI::Client.new(:api_key => ("OPENAI_API_KEY"))
+  request_headers_hash = {
+  "Authorization" => "Bearer #{ENV.fetch("OPENAI_API_KEY")}",
+  "content-type" => "application/json"
+}
+
+request_body_hash = {
+  "model" => "gpt-3.5-turbo",
+  "messages" => [
+    {
+      "role" => "system",
+      "content" => "You help food and craft vendors find events that are currently accepting applications for vendors. You provide a list of 5 items to start with clickable links to the event pages and applications. You ask them is they are interested in a specific type of event or any. You ask if they have a location preference or not. You ask them if they have a specific date range in mind or not."
+    },
+    {
+      "role" => "user",
+      "content" => "Generate a list of events to choose from."
+    }
+  ]
+}
+
+question = params.fetch("user_question") 
+@asked_question = question 
+
+request_body_json = JSON.generate(request_body_hash)
+
+raw_response = HTTP.headers(request_headers_hash).post(
+  "https://api.openai.com/v1/chat/completions",
+  :body => request_body_json
+).to_s
+
+parsed_response = JSON.parse(raw_response)
+@info_string = parsed_response.fetch("choices")
+
+ info_string = parsed_response.fetch("choices")
+  information = info_string.at(0)
+  message = information.fetch("message")
+  @reply = message.fetch("content")
+
+
+
+
+erb(:events_chat, {:layout => :layout})
+
+
+
+
+
+
+
+
+
+
+
+  # message_list = [
+  #   { "role" => "system", "content" => "You help food and craft vendors find events that are currently accepting applications for vendors. You provide a list of 5 items to start with clickable links to the event pages and applications. You ask them is they are interested in a specific type of event or any. You ask if they have a location preference or not. You ask them if they have a specific date range in mind or not." },
+  #   { "role" => "user", "content" => "Generate a list of events to choose from." }
+  # ]
+
+  # api_response = client.chat(
+  #   parameters: {
+  #     model: "gpt-3.5-turbo",
+  #     messages:  message_list
+  #   }
+  # )  
+  
+  
 end
